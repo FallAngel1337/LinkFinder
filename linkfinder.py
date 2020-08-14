99c978153093f092b06169868505ee63308329ad
FallAngel1337 <62574781+FallAngel1337@users.noreply.github.com>
2020-08-14 10:30:11 -0300

    Fix_LinkFinder

--- linkfinder.py
+++ linkfinder.py
@@ -8,17 +8,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--url', help='The target url')
 parser.add_argument('-f', help='The files with the urls')
 parser.add_argument('-o', help='The output file')
-parser.add_argument('--save_dir', help='It will creat a dir to save the output', action='store_true')
 parser.add_argument('--comments', help='Grab the comments on the page', action='store_true')
 args = parser.parse_args()
 
 if args.f:
-    if args.save_dir:
-        try:
-            os.mkdir('LinkFinder')
-            os.chdir('LinkFinder')
-        except FileExistsError:
-            os.chdir('LinkFinder')
+    try:
+        os.mkdir('LinkFinder')
+        os.chdir('LinkFinder')
+    except FileExistsError:
+        os.chdir('LinkFinder')
 
     with open(args.f, 'r') as urls:
         for url in urls:
@@ -27,40 +25,41 @@ if args.f:
             try:
                 resp = requests.get(url).text
                 soup = BeautifulSoup(resp, 'html.parser')
-                
-                if args.save_dir:
+
+                if args.comments:
+                        print(f'{"=" * 20} {url} {"=" * 20}\n')
+                        for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
+                            with open(url.replace('http://', '').replace('https://', '')+'_comments', 'a+') as output:
+                                for link in soup.find_all('a', href=True):
+                                    print(link['href'])
+                                    output.write(link['href']+'\n')
+                else:
                     with open(url.replace('http://', '').replace('https://', ''), 'a+') as output:
                         for link in soup.find_all('a', href=True):
                             print(link['href'])
                             output.write(link['href']+'\n')
-                elif args.comments:
-                        print(f'{"=" * 20} {url} {"=" * 20}\n')
-                        for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
-                            print(comment, '\n')
-                else:
-                    for link in soup.find_all('a', href=True):
-                        print(link['href'])
             except ConnectionRefusedError:
                 print('Connection Refused at', url)
             except Exception as err:
                 print('Something gone WRONG!')
                 print(err)
-                #exit()
-else:
+elif args.url:
     try:
         resp = requests.get(args.url).text
         soup = BeautifulSoup(resp, 'html.parser')
         
-        if args.o:
+        if args.o and args.comments:
+            print(f'{"=" * 20} {args.url} {"=" * 20}\n')
+            with open(args.o, 'a+') as output:
+                for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
+                    print(comment, '\n')
+                    output.write(comment+'\n')
+
+        elif args.o:
             with open(args.o, 'w+') as output:
                 for link in soup.find_all('a', href=True):
                     print(link['href'])
                     output.write(link['href']+'\n')
-        elif args.comments:
-            print(f'{"=" * 20} {url} {"=" * 20}\n')
-            for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
-                print(comment,'\n')
-            print('=' * 45)
         else:
             for link in soup.find_all('a', href=True):
                 print(link['href'])
@@ -69,5 +68,3 @@ else:
     except Exception as err:
         print('Somthing gone WRONG!')
         print(err)
-        #exit()
-
